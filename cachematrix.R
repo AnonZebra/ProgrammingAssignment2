58a38e6d52bb81b8a4df14fa71543552b4942ef3
AnonZebra <bestevater@posteo.de>
2019-07-17 16:06:00 +0200

    Template solution in cachematrix.R replaced with my own solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,38 @@
 ## Functions for a) storing and retrieving a matrix, and b) calculating and retrieving
 ## the inverse of said matrix.
 
-## A function that produces a list of four functions, for 1) storing a matrix,
-## 2) retrieving said matrix, 3) storing the inverse of said matrix and 
-## 4) retrieving the inversed matrix.
-
-makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function (y) { # 1)
-    x <<- y
-    i <<- NULL
+makeCacheMatrix <- function(cachm = matrix()) {
+  cachs <- NULL
+  set_cachm <- function(y) {
+    cachm <<- y
+    cachs <<- NULL
   }
-  get <- function() x # 2)
-  setinv <- function(inv) i <<- inv # 3)
-  getinv <- function() i # 4)
-  list(set = set, get = get,
-       setinv = setinv, 
-       getinv = getinv)
+  
+  get_cachm <- function() cachm
+  
+  set_cachs <- function(solvmat) {
+    cachs <<- solvmat
+  }
+  
+  get_cachs <- function() {
+    return(cachs)
+  }
+  
+  return(
+    list(set_cachm=set_cachm, get_cachm=get_cachm,
+         set_cachs=set_cachs, get_cachs=get_cachs)
+  )
+  
 }
 
-
-## This function checks to see if the passed argument/object has a cached inverse matrix
-## object, otherwise it uses the 'get function' to retrieve the original matrix and
-## calculate the inverse, and then returns it.
-
-cacheSolve <- function(x, ...) {
-  te <- x$getinv()
-  if(!is.null(te)) {
-    print('getting cached data')
-    return(te)
+cacheSolve <- function(cmat) {
+  sma <- cmat$get_cachs()
+  if (!is.null(sma)) {
+    message("getting cached data")
+    return(sma)
   }
-  te <- solve(x$get())
-  return(te)
+  sma <- solve(cmat$get_cachm())
+  cmat$set_cachs(sma)
+  return(sma)
 }
+
